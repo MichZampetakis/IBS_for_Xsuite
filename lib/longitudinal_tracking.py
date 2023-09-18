7286c95c2f613817f82491c5108608d285e9440a
Felix Soubelet <felix.soubelet@protonmail.com>
2023-09-18 17:13:37 +0200

    formatting, should maybe be modules in pyibs

--- lib/longitudinal_tracking.py
+++ lib/longitudinal_tracking.py
@@ -1,29 +1,31 @@
 import numpy as np
 
+# TODO: this is not called elsewhere, can go into some utils module in pyibs
+
 def drift(eta, C0, z, delta):
     z1 = z - eta * delta * C0
     delta1 = delta
     return z1, delta1
 
-def RF_map(P0, E0, beta_0, f_RF, c, lag, V_RF, En, z, delta):
 
-    P = (1 + delta) * P0 # GeV/c
-    gamma = np.sqrt(1 + (P * c / E0)**2)
+def RF_map(P0, E0, beta_0, f_RF, c, lag, V_RF, En, z, delta):
+    P = (1 + delta) * P0  # GeV/c
+    gamma = np.sqrt(1 + (P * c / E0) ** 2)
     beta = np.sqrt(1 - 1 / gamma**2)
     old_rvv = beta / beta_0
 
     # RF energy kick
-    k = 2 * np.pi * f_RF  / c
+    k = 2 * np.pi * f_RF / c
     tau = z / old_rvv / beta_0
-    phase = lag * np.pi / 180. - k * tau
+    phase = lag * np.pi / 180.0 - k * tau
     energy = V_RF * np.sin(phase)
 
     deltabeta_0 = delta * beta_0
     ptaubeta_0 = np.sqrt(deltabeta_0**2 + 2 * deltabeta_0 * beta_0 + 1) - 1
-    ptaubeta_0 += energy / En # energy kick
+    ptaubeta_0 += energy / En  # energy kick
     ptau = ptaubeta_0 / beta_0
 
-    delta1 = np.sqrt(ptau ** 2 + 2 * ptau / beta_0 + 1) - 1
+    delta1 = np.sqrt(ptau**2 + 2 * ptau / beta_0 + 1) - 1
     rvv = (1 + delta1) / (1 + ptaubeta_0)
     z1 = z * rvv / old_rvv
     return z1, delta1
--- lib/simulation_parameters.py
+++ lib/simulation_parameters.py
@@ -1,33 +1,34 @@
 import numpy as np
-from scipy.constants import c, epsilon_0, e, hbar, m_p
-from scipy.constants import physical_constants
+from scipy.constants import c, e, epsilon_0, hbar, m_p, physical_constants
 
-#r0 = physical_constants["classical electron radius"][0]                     #Electron Classical Radius [m]
-E0p = physical_constants["proton mass energy equivalent in MeV"][0]*1e-3   #Proton Rest Mass [Gev]
+# TODO: this is not called elsewhere, can go into some constants module in pyibs
 
-Am  = 208.0          #Atomic Mass (207.2)
-Z   = 54.0           #Number of Charges
-Nb  = 2e8            #Bunch Population
-h   = 2              #Harmonic Number
+# r0 = physical_constants["classical electron radius"][0]                     #Electron Classical Radius [m]
+E0p = physical_constants["proton mass energy equivalent in MeV"][0] * 1e-3  # Proton Rest Mass [Gev]
 
-E0i = 193.6999       #Rest energy in GeV
-Ek  = 0.0042         #Kinetic Energy  [GeV]
-En = E0i + Ek * Am   #Ion Total Energy [GeV]
-P0  = np.sqrt(En**2 - E0i**2) / c #Reference momentum [GeV/c]
-mi  = (E0i * m_p) / E0p   #Ions Mass
+Am = 208.0  # Atomic Mass (207.2)
+Z = 54.0  # Number of Charges
+Nb = 2e8  # Bunch Population
+h = 2  # Harmonic Number
+
+E0i = 193.6999  # Rest energy in GeV
+Ek = 0.0042  # Kinetic Energy  [GeV]
+En = E0i + Ek * Am  # Ion Total Energy [GeV]
+P0 = np.sqrt(En**2 - E0i**2) / c  # Reference momentum [GeV/c]
+mi = (E0i * m_p) / E0p  # Ions Mass
 
 gamma_rel = En / E0i
-beta_rel  = np.sqrt(1 - 1/gamma_rel**2)
+beta_rel = np.sqrt(1 - 1 / gamma_rel**2)
 
 gamma_tr = 2.838351
-a_p      = 1 / gamma_tr**2
-SlipF    = (1/gamma_tr**2) - (1 / gamma_rel**2)  #Slip Factor
+a_p = 1 / gamma_tr**2
+SlipF = (1 / gamma_tr**2) - (1 / gamma_rel**2)  # Slip Factor
 
-V0 = 1.1e-6     #RF-Voltage [GV]
-U0 = 0.0        #Energy Loss / Turn [Gev]
-p_increment = U0 * 1e+9 * 1.602 * 1e-19 / c    # kg*m/s //turn
+V0 = 1.1e-6  # RF-Voltage [GV]
+U0 = 0.0  # Energy Loss / Turn [Gev]
+p_increment = U0 * 1e9 * 1.602 * 1e-19 / c  # kg*m/s //turn
 
-r0  = (Z * e)**2 / (4 * np.pi * epsilon_0 * c**2 * mi)  #Ion Classical Radius
+r0 = (Z * e) ** 2 / (4 * np.pi * epsilon_0 * c**2 * mi)  # Ion Classical Radius
 
 # ! -------------------------------------------- !
 
@@ -35,9 +36,9 @@ turns = 16000
 IBS_step = 50
 
 # ! ---------- Equilibrium ------------ !
-emit_x0  = 2.95814809e-6
-emit_y0  = 2.95814809e-6
+emit_x0 = 2.95814809e-6
+emit_y0 = 2.95814809e-6
 Nemit_x0 = emit_x0 * beta_rel * gamma_rel
 Nemit_y0 = emit_y0 * beta_rel * gamma_rel
 bl0 = 4.256474951
-#Sigma_E0 = 1.05834e-05
+# Sigma_E0 = 1.05834e-05
