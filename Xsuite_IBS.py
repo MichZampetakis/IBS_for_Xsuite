328345415037e0adb123d0c7ec1d1e684d74106e
Felix Soubelet <felix.soubelet@protonmail.com>
2023-09-19 11:26:54 +0200

    cleanup and parametrize

--- Xsuite_IBS.py
+++ Xsuite_IBS.py
@@ -1,181 +1,177 @@
-import json
-import sys
+from dataclasses import dataclass
+from pathlib import Path
 
-import matplotlib.pyplot as plt
+import click
 import numpy as np
 import xobjects as xo
 import xpart as xp
 import xtrack as xt
 from cpymad.madx import Madx
-from scipy.constants import e as qe
-from scipy.constants import m_e, m_p
+from loguru import logger
 
-# from lib.Xsuite_eval_emit_sig import *
-# from lib.general_functions import *
-from lib.IBSfunctions import *
+from lib.IBSfunctions import NagaitsevIBS
 
-## Read a MAD-X Sequence
-n_slice_per_element = 4
-mad = Madx(stdout=False)
-mad.call("./chrom-corr_DR.newlattice_2GHz.seq")
-mad.input(
-    f"""
-beam, particle=positron, energy=2.86, bunched;
-use, sequence=RING;"""
+
+@dataclass
+class Records:
+    """Dataclass to store (and update) important values through tracking."""
+
+    epsilon_x: np.ndarray
+    epsilon_y: np.ndarray
+    sig_delta: np.ndarray
+    bl: np.ndarray
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option("--nturns", default=1000, type=int, show_default=True, help="Number of turns to track for.")
+@click.option(
+    "--sequence",
+    required=False,
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    help="Path to a file with the MAD-X sequence to load.",
 )
-twthick = mad.twiss().dframe()
-mad.input(
-    f"""
-select, flag=MAKETHIN, SLICE={n_slice_per_element}, thick=false;
-select, flag=MAKETHIN, pattern=wig, slice=1;
-MAKETHIN, SEQUENCE=ring, MAKEDIPEDGE=true;
-use, sequence=RING;"""
+@click.option(
+    "--line",
+    required=False,
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    help="Path to a JSON file with the line to load.",
 )
-twthin = mad.twiss().dframe()
-line = xt.Line.from_madx_sequence(mad.sequence["RING"])
-
-# Need to find if the aperture is changed in Xsuite!!!!!! #
-# line.insert_element(idx = 0, # start of the ring
-#                     element = xt.LimitRect(min_x = -0.04, max_x = 0.04, min_y = -0.04, max_y = 0.04),
-#                     name = 'aperture')
-
-
-## Choose a context
-context = xo.ContextCpu()  # For CPU
-
-## Transfer lattice on context and compile tracking code
-tracker = xt.Tracker(_context=context, line=line)
-
-cavs = [ee for ee in line.elements if isinstance(ee, xt.Cavity)]
-for cc in cavs:
-    cc.lag = 180
-    # if cc.voltage > 0:
-    #    cc.voltage = -cc.voltage
-
-p0 = xp.Particles(mass0=xp.ELECTRON_MASS_EV, q0=1, p0c=2.86e9)
-tw = line.twiss(particle_ref=p0)
-
-bunch_intensity = 4.4e9
-sigma_z = 1.58e-3
-n_part = int(5e3)
-nemitt_x = 5.6644e-07
-nemitt_y = 3.7033e-09
-particles = xp.generate_matched_gaussian_bunch(
-    num_particles=n_part,
-    total_intensity_particles=bunch_intensity,
-    nemitt_x=nemitt_x,
-    nemitt_y=nemitt_y,
-    sigma_z=sigma_z,
-    particle_ref=p0,
-    tracker=tracker,
+@click.option(
+    "--model",
+    type=click.Choice(["Simple", "Kinetic", "Nagaitsev"], case_sensitive=False),
+    show_default=True,
+    default="Simple",
+    help="The IBS model to use.",
 )
+@click.option(
+    "--outputdir",
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+    default=".",
+    show_default=True,
+    help="Folder in which to write the output data.",
+)
+def main(nturns: int, sequence: Path, line: Path, model: str, outputdir: Path) -> None:
+    """Main program flow."""
+    # ----- Create xtrack Line ----- #
+    logger.info("Creating Line for tracking")
+    if not sequence and not line:
+        raise ValueError("At least one of the MAD-X sequence or line JSON is required")
+    if sequence and not line:
+        line: xt.Line = line_from_madx(sequence.absolute())
+    else:  # we have the line
+        if sequence:  # but also the sequence
+            logger.debug("Both MAD-X sequence and JSON line file provided, prioritizing the line")
+        line: xt.Line = line_from_json(line.absolute())
 
-# ----- Initialize IBS -----
-IBS = NagaitsevIBS()
-IBS.set_beam_parameters(particles)
-IBS.set_optic_functions(tw)
+    # ----- Choose context and build tracker ----- #
+    logger.info("Compiling kernels")
+    context = xo.ContextCpu()  # For CPU
+    tracker = line.build_tracker(_context=context)
 
-# emit = Evaluate_Sigma_Emit(10,20,10,20,20,20)
-# emit.define_bins_width(particles, tw)
+    # ----- Prepare acceleration ----- #
+    logger.info("Activating cavities")
+    cavities = [element for element in line.elements if isinstance(element, xt.Cavity)]
+    logger.debug(f"Found {len(cavities)} to activate (lag = 180)")
+    for cavity in cavities:
+        cavity.lag = 180
 
-## Initialize dictionaries
-n_turns = 1000
-turn_by_turn = {}
+    # ----- Particles and Twiss ----- #
+    logger.info("Getting line Twiss and generating particles for tracking")
+    bunch_intensity = 4.4e9
+    sigma_z = 1.58e-3
+    n_part = int(5e3)
+    nemitt_x = 5.6644e-07
+    nemitt_y = 3.7033e-09
+    p0 = xp.Particles(mass0=xp.ELECTRON_MASS_EV, q0=1, p0c=2.86e9)
+    particles = xp.generate_matched_gaussian_bunch(
+        num_particles=n_part,
+        total_intensity_particles=bunch_intensity,
+        nemitt_x=nemitt_x,
+        nemitt_y=nemitt_y,
+        sigma_z=sigma_z,
+        particle_ref=p0,
+        tracker=tracker,
+    )
+    twiss = line.twiss(particle_ref=p0)
 
-# record_coord = ['x', 'px', 'y', 'py', 'zeta', 'delta', 'state', 'particle_id']
-# for nn in record_coord:
-#    turn_by_turn[nn] = np.zeros((n_turns, n_part), dtype = getattr(particles, nn).dtype)
+    # ----- Initialize IBS ----- #
+    logger.info("Initializing Nagaitsev IBS class")
+    IBS = NagaitsevIBS()
+    IBS.set_beam_parameters(particles)
+    IBS.set_optic_functions(twiss)
 
-record_emit = ["eps_x", "eps_y", "sig_delta", "bl"]
-for nn in record_emit:
-    turn_by_turn[nn] = np.zeros((n_turns), dtype=float)
-print("hahaha")
-## Track (saving turn-by-turn data)
-for i in range(n_turns):
-    print("Turn = ", i)
-    print("N_part = ", len(particles.x[particles.state > 0]))
+    # ----- Initialze Records ----- #
+    turn_by_turn = Records(
+        epsilon_x=np.zeros(nturns, dtype=float),
+        epsilon_y=np.zeros(nturns, dtype=float),
+        sig_delta=np.zeros(nturns, dtype=float),
+        bl=np.zeros(nturns, dtype=float),
+    )
 
-    #    for nn in record_coord:
-    #        turn_by_turn[nn][i,:] = getattr(particles, nn)
+    # ----- Tracking ----- #
+    logger.info(f"Stating tracking for {nturns}")
+    for turn in range(nturns):
+        logger.trace("Computing particle properties")
+        sig_x = np.std(particles.x[particles.state > 0])
+        sig_y = np.std(particles.y[particles.state > 0])
+        sig_delta = np.std(particles.delta[particles.state > 0])
+        turn_by_turn.bl[turn] = np.std(particles.zeta[particles.state > 0])
+        turn_by_turn.sig_delta[turn] = sig_delta
+        turn_by_turn.epsilon_x[turn] = (sig_x**2 - (twiss["dx"][0] * sig_delta) ** 2) / twiss["betx"][0]
+        turn_by_turn.epsilon_y[turn] = sig_y**2 / twiss["bety"][0]
+        # turn_by_turn.epsilon_x[turn], turn_by_turn.epsilon_y[turn], turn_by_turn.sig_delta[turn], turn_by_turn.bl[turn] = emit.eval_emits(particles)
 
-    sig_x = np.std(particles.x[particles.state > 0])
-    sig_y = np.std(particles.y[particles.state > 0])
-    sig_delta = np.std(particles.delta[particles.state > 0])
-    turn_by_turn["bl"][i] = np.std(particles.zeta[particles.state > 0])
-    turn_by_turn["sig_delta"][i] = sig_delta
-    turn_by_turn["eps_x"][i] = (sig_x**2 - (tw["dx"][0] * sig_delta) ** 2) / tw["betx"][0]
-    turn_by_turn["eps_y"][i] = sig_y**2 / tw["bety"][0]
-    # turn_by_turn['eps_x'][i], turn_by_turn['eps_y'][i], turn_by_turn['sig_delta'][i], turn_by_turn['bl'][i] = emit.eval_emits(particles)
+        if turn % 50 == 0:
+            logger.debug(
+                f"Turn {turn} / {nturns}, {len(particles.x[particles.state > 0])} surviving particles"
+            )
+            logger.trace(f"At turn {turn}, re-calculating IBS {model.capitalize()} kick")
+            if model.lower() == "simple":
+                IBS.calculate_simple_kick(particles)
+            elif model.lower() == "kinetic":
+                IBS.calculate_kinetic_coefficients(particles)
 
-    if i % 50 == 0:
-        IBS.calculate_simple_kick(particles)
-    IBS.apply_simple_kick(particles)
+        # ----- Applying relevant kick to particles ----- #
+        if model.lower() == "simple":
+            IBS.apply_simple_kick(particles)
+        elif model.lower() == "kinetic":
+            IBS.apply_kinetic_kick(particles)
 
-    # if (i % 50 == 0):
-    #     IBS.calculate_kinetic_coefficients(particles)
-    # IBS.apply_kinetic_kick(particles)
+        # ----- Track 1 turn through line ----- #
+        line.track(particles)
 
-    line.track(particles)
-#    rec_id = turn_by_turn['particle_id'].copy()
-#    for nn in record_coord:
-#        temp = turn_by_turn[nn].copy()
-#        turn_by_turn[nn][:, rec_id] = temp
-
-Emitt = []
-Emitt.append(turn_by_turn["eps_x"])
-Emitt.append(turn_by_turn["eps_y"])
-Emitt.append(turn_by_turn["sig_delta"])
-Emitt.append(turn_by_turn["bl"])
-np.savetxt(r"./output/xsuite_simple.txt", np.array(Emitt).T.tolist(), fmt="%e")
-
-sys.exit()
-
-n_turns = 100
-tracker.track(particles, num_turns=n_turns, turn_by_turn_monitor=True)
-
-## Turn-by-turn data is available at:
-tracker.record_last_track.x
-tracker.record_last_track.px
-sys.exit()
-
-print(tracker.record_last_track.x)
-print(tracker.line.element_names)
-# print(tracker.line.elements[0])
+    # ----- Saving data to file ----- #
+    logger.debug("Saving recorded data to file")
+    np.savez(
+        outputdir / f"xsuite_{model.lower()}.npz",
+        epsilon_x=turn_by_turn.epsilon_x,
+        epsilon_y=turn_by_turn.epsilon_y,
+        sig_delta=turn_by_turn.sig_delta,
+        bl=turn_by_turn.bl,
+    )
 
 
-## Track (saving turn-by-turn data)
-# loop with 1 turn .. to evaluate IBS etc
-# (later) or append 1 element in line to do the IBS kick
+def line_from_madx(sequence_file: Path) -> xt.Line:
+    """Function to load the line from MAD-X (hardcoded behavior)."""
+    logger.debug("Loading line from MAD-X")
+    n_slice_per_element = 4
+    madx = Madx(stdout=False)
+    madx.call(sequence_file)
+    madx.command.beam(particle="positron", energy=2.86, bunched=True)
+    madx.command.use(sequence="RING")
+    madx.command.select(flag="MAKETHIN", slice_=n_slice_per_element, thick=False)
+    madx.command.select(flag="MAKETHIN", pattern="wig", slice_=1)
+    madx.command.makethin(sequence="RING", makedipedge=True)
+    madx.command.use(sequence="RING")
+    logger.debug("Converting MAD-X sequence to xtrack line")
+    return xt.Line.from_madx_sequence(madx.sequence.ring)
 
-# rec_sig_x = np.zeros(n_turns, dtype = np.float)
-# rec_sig_delta = np.zeros(n_turns, dtype = np.float)
 
-"""
-f, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(16,5))
-ax1.plot(particles.x, particles.px, '.')
-ax2.plot(particles.y, particles.py, '.')
-ax3.plot(particles.zeta, particles.delta, '.')
-plt.show()
-"""
+def line_from_json(json_file: Path) -> xt.Line:
+    """Function to load the line directly from a JSON file."""
+    logger.debug("Loading line from MAD-X")
+    return xt.Line.from_json(json_file)
 
-""" # Radiation if off in the multipoles
-myline = line.to_dict()
-for i in range(len(myline['element_names'])):
-    if myline["elements"][i]["__class__"] == "Multipole":
-        if myline["elements"][i]["radiation_flag"] != 0 : print('Radiation!')
-"""
 
-"""
-# Particle at (0,0)x3 remains stable.
-n_part = 1
-particles = xp.Particles(_context=context, p0c=2.86e9,
-                         x=0, px=0, y=0, py=0, zeta=0, delta=0, )
-## Track (saving turn-by-turn data)
-n_turns = 100
-tracker.track(particles, num_turns=n_turns,
-              turn_by_turn_monitor=True)
-
-## Turn-by-turn data is available at:
-tracker.record_last_track.x
-tracker.record_last_track.px
-"""
+if __name__ == "__main__":
+    main()
