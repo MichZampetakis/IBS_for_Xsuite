e810892a8a34f53c0cf6fff0b2cf32317c905029
Felix Soubelet <felix.soubelet@protonmail.com>
2023-09-26 12:02:00 +0200

    add xibs.evolutions, with docstring

---
+++ xibs/evolutions.py
@@ -0,0 +1,9 @@
+"""
+.. _xibs-analytical:
+
+Analytical IBS Calculations
+---------------------------
+
+Module with user-facing API to compute IBS-induced emittance evolutions from Nagaitsev integrals.
+"""
+import numpy as np
