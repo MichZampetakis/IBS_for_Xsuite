5a6127f4e255625010061025ab905eb73f12a958
Felix Soubelet <felix.soubelet@protonmail.com>
2023-09-26 12:01:30 +0200

    add xibs.xsimple, with docstring

---
+++ xibs/simple.py
@@ -0,0 +1,9 @@
+"""
+.. _xibs-simple:
+
+Simple Kick IBS Formalism
+-------------------------
+
+Module with user-facing API to compute simple IBS kicks from the Nagaitsev integrals and apply them to particles.
+"""
+import numpy as np
