5f3bf21c5b9a16e75d3739e4d5a0d389093ebcb5
Felix Soubelet <felix.soubelet@protonmail.com>
2023-09-26 12:00:42 +0200

    expand docstring

--- xibs/kinetic.py
+++ xibs/kinetic.py
@@ -4,6 +4,6 @@
 Kinetic IBS Formalism
 ---------------------
 
-Module with user-facing API to compute diffusion and friction terms from the Nagaitsev integrals according to the kinetic formalism, and apply the corresponding IBS kicks to particles.
+Module with user-facing API to compute diffusion and friction terms from the Nagaitsev integrals according to the kinetic formalism, as well as the corresponding kinetic IBS kicks and apply them to particles.
 """
 import numpy as np
