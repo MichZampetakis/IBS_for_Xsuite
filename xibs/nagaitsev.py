f8d99a99ff82fddfa871dc4ae7a66d1efbdcaf2b
Felix Soubelet <felix.soubelet@protonmail.com>
2023-09-26 11:47:56 +0200

    add nagaitsev module which will get you the integrals

---
+++ xibs/nagaitsev.py
@@ -0,0 +1,11 @@
+"""
+.. _xibs-nagaitsev:
+
+Analytical Nagaitsev Calculations
+----------------------------------
+
+Module with functionality to perform analytical calculations according to Nagaitsev's formalism.
+A user-facing class is provided which computes the Nagaitsev integrals based on beam optics.
+The formalism from which formulas and calculations are implemented can be found in :cite:`PRAB:Nagaitsev:IBS_formulas_fast_numerical_evaluation`.
+"""
+import numpy as np
